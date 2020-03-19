1c209c8ddbf0825ef80ba2a45a1ce6b754cccaa0
LazarevIhor <ihor.lazarev@smartexe.com>
2020-03-19 12:21:45 +0200

    grid init commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+  <Summernote />
+  </div>
+</template>
+
+<script>
+  import Summernote from './assets/components/summernote';
+
+export default {
+  name: 'app',
+  components: {Summernote},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
